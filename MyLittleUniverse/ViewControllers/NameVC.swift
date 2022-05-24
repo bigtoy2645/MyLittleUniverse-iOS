3c01b5a3cc8332ceaca8c29712d43a5449b00764
yurim <bigtoy2645@naver.com>
2022-05-24 22:16:41 +0900

    fix: check user name validation

--- MyLittleUniverse/ViewControllers/NameVC.swift
+++ MyLittleUniverse/ViewControllers/NameVC.swift
@@ -11,7 +11,14 @@ import RxCocoa
 
 class NameVC: UIViewController {
     let length = BehaviorRelay<Int>(value: 0)
+    let validation = BehaviorRelay<RegexResult>(value: .ko)
     let isValid = BehaviorRelay<Bool>(value: false)
+    
+    enum RegexResult: String {
+        case ko = "한글만 입력 가능합니다."
+        case length = "최대 한글 12글자 이내로 입력 가능합니다."
+        case verified = ""
+    }
     
     private let maxLength = 12
     private let disposeBag = DisposeBag()
@@ -30,27 +37,37 @@ class NameVC: UIViewController {
     func setupBindings() {
         txtName.rx.text.map { text in
             guard let text = text else { return 0 }
-            return text.count
+            return text.utf16.count
         }
         .subscribe(onNext: length.accept(_:))
         .disposed(by: disposeBag)
         
-        txtName.rx.text.map { text in
-            guard let text = text else { return true }
-            let lengthValidation = (text.count > 0 && text.count <= self.maxLength)
-            let regexValidation = self.checkNamePolicy(text: text)
-            return lengthValidation && regexValidation
-        }
+        validation.map { $0 == .verified }
             .subscribe(onNext: isValid.accept(_:))
             .disposed(by: disposeBag)
         
-        length.map { self.isValid.value || $0 == 0 }
+        txtName.rx.text.map { text in
+            guard let text = text else { return .verified }
+            let lengthValidation = (text.utf16.count > 0 && text.utf16.count <= self.maxLength)
+            let regexValidation = self.checkNamePolicy(text: text)
+            if !lengthValidation        { return .length }
+            else if !regexValidation    { return .ko }
+            return .verified
+        }
+        .subscribe(onNext: validation.accept(_:))
+        .disposed(by: disposeBag)
+        
+        validation.map { $0.rawValue }
+            .bind(to: lblError.rx.text)
+            .disposed(by: disposeBag)
+        
+        isValid.map { $0 || self.length.value == 0 }
             .bind(to: lblError.rx.isHidden)
             .disposed(by: disposeBag)
         
-        length.map {
-            if $0 == 0 { return .gray300 }
-            return self.isValid.value ? .black : .errorRed
+        isValid.map {
+            if self.length.value == 0 { return .gray300 }
+            return $0 ? .black : .errorRed
         }
         .bind(to: underLineView.rx.backgroundColor)
         .disposed(by: disposeBag)
